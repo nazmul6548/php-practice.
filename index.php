7006caf5a06fe722d304c29fdab21eb5036e2874
nazmul6548 <nazmul6548@gmail.com>
2023-11-16 23:54:50 +0600

    php

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
-var_dump((bool) array ());
+// boolean number.
+// var_dump((bool) array ());
+
+
+// flot number.
+// var_dump (25/7);
+
+// flot to int
+// var_dump ((int) (25/7));
+
+// string
+// echo "name";
+// echo 'nazmul';
+
+// echo 'my name is jewel,i'll be back';
+// right way
+// echo 'my name is jewel,i\'ll be back';
+// $var = "polash";
+// echo "my name is $var";
+// $var = "polash";
+// echo 'my name is $var';
+// $var = "polash";
+// echo 'my name is '. $var;
+// $var = "polash";
+// echo 'my name is '." ". $var;
