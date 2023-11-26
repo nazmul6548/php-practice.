cb8a34fd3a9d303581f344c1cefa84634cd977ec
nazmul6548 <nazmul6548@gmail.com>
2023-11-26 11:11:56 +0600

    foor in loof

---
+++ forInLoof.js
@@ -0,0 +1,15 @@
+// const number = [1, 2, 3, 4, 5, 6];
+// for (const numbers in number) {
+//     console.log(number[numbers]);
+        
+//     }
+let myName = { name: 'nazmul', age:25, email:'nazmul6548@gmail.com'};
+for (const result in myName) {
+   console.log(result);
+    }
+
+
+    let myAdress = { name: 'nazmul', age:25, email:'nazmul6548@gmail.com'};
+for (const result in myAdress) {
+   console.log(myAdress[result]);
+    }
--- foreach.js
+++ foreach.js
@@ -4,3 +4,4 @@ let nums = ["a", "b", "c", "d"];
 
 const results = nums.findIndex((text) => text==="d" );
 console.log(results);
+
